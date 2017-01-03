61016ffaa2a57b2de5032b68363669ca58e005b5
TomORPLeX <tom.butterworth@openreach.co.uk>
2017-01-03 10:51:25 +0000

    tom 6

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 var loginfunction = require("../bin/login.js");
 
+obj = {'db' : "" };
+
 router.get('/', loginfunction.isLoggedIn, function(req, res, next) {
-    res.render('admin');
+    res.render('admin',obj);
 });
 
 module.exports = router;
