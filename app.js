4d704b3de9faf8f44acb55556452a4580e87805a
TomORPLeX <tom.butterworth@openreach.co.uk>
2017-01-03 12:12:24 +0000

    tom 7

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser(''));
+app.use(cookieParser('mysecrethere'));
 
 app.use('/', index);
 app.use('/logincheck', logincheck);
