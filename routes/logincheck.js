5774ba5b42c9f010930f1ee69cd5b1afff303827
PLeXORNS <adam.ainsworth@openreach.co.uk>
2017-01-10 16:34:16 +0000

    10/01/17 16:34 adam1

--- routes/logincheck.js
+++ routes/logincheck.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../bin/db.js');
+var fs = require('fs');
 
 router.use('/', function(req, res, next) {
 
     var EIN = req.body.EIN;
     var quer1 = "SELECT * FROM users WHERE EIN = '"+EIN+"'";
     var obj = {};
+
+
 
     pool.query(quer1, function(err,rows)
     {
@@ -37,6 +40,25 @@ router.use('/', function(req, res, next) {
             res.cookie('loginFlag', loginFlag1, { maxAge: 900000, httpOnly: false});
             res.cookie('adminFlag', adminFlag1, { maxAge: 900000, httpOnly: false});
             res.cookie('cases','', { maxAge: 900000, httpOnly: false});
+            //create temp file for map
+            var initialJson = {"LatLngData":
+                                    [{"LON":"",
+                                        "LAT":"",
+                                        "PRIMARY_SKILL":"",
+                                        "WT_DESCRIPTION":"",
+                                        "CASE_STATUS":"",
+                                        "PRIORITY_DESCRIPTION":"",
+                                        "EXCH":"",
+                                        "CASE_ID":"",
+                                        "CUST_EST_NO":""}],
+                                "selection":
+                                        {"fluidity":"All",
+                                            "planned":"All",
+                                            "priority":[""],
+                                            "skills":[""],
+                                            "ouc":"All"}};
+            var tempfilelocation = '../public/data/' +EIN +'_LatLngData.json';
+            fs.writeFile(tempfilelocation, JSON.stringify(initialJson) );
 
 
             //res.render('index', { title: 'Welcome '+ req.cookies.username, loginFlag: req.cookies.loginFlag, adminFlag: req.cookies.adminFlag });
