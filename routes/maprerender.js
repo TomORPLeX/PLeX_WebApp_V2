10f6329c6c2c61eb753e2a8b8fb3949035fc95e4
PLeXORNS <adam.ainsworth@openreach.co.uk>
2017-02-21 13:57:30 +0000

    adam task duration change and error logs 21/02/17

--- routes/maprerender.js
+++ routes/maprerender.js
@@ -124,8 +124,8 @@ router.all('/',loginfunction.isLoggedIn,function(req,res,next) {
     //Filter out jobs in execute
     var quer5 = "SELECT  LON, LAT, WEB_PRIMARY_SKILL AS PRIMARY_SKILL, JOBDESCRIPTION, SUB_DESCRIPTION, web_system_defined_priority, FINAL_STATUS as CASE_STATUS, EXCHANGE, CASE_ID, ESTIMATENUMBER, CASE_OBJID, QUEUE_ID FROM live_workstack " + dataString + ";";
     var quer6 = "SELECT COUNT(*) as Total, priority_description as priority_description FROM live_workstack " + dataString + "group by priority_description;";
-    console.log(quer5);
-    console.log(data.durationinput);
+    //console.log(quer5);
+    //console.log(data.durationinput);
 
     pool.query(quer5, function (err,rows) {
         if (err) {
